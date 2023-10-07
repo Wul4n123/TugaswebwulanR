e411d2b583ca1f6613d761428b708598d6843bcf
Wul4n123 <144516535+Wul4n123@users.noreply.github.com>
2023-10-07 10:25:14 +0700

    Update walimurid.php

--- walimurid.php
+++ walimurid.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>walimurid</title>
+</head>
+<body>
+   <?
+   session_start();
 
+   // cek apakah yang mengakses halaman ini sudah login
+if($_SESSION['level']==""){
+   header("location:index.php? pesan=gagal");
+}
+
+?>
+<h1>walimurid</h1>
+
+<p>Halo <b><?php  echo $_SESSION['username']; ?><b> Anda telah login sebagai <b>
+<? echo $_SESSION['level']; ?></b>.</p>
+<a href="logout.php">LOGOUT</a>
+
+<br/>
+<br/>
+
+<a>Membuat Login Multi Level Dengan PHP</a>
+
+</body>
+</html>
